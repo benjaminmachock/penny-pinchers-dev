4fc5ae9d99864ee8e197ea0a0a85e1cae5a68342
Benjamin Machock <43613715+benjaminmachock@users.noreply.github.com>
2024-11-24 11:56:17 -0700

    added typeDefs for Cart and CartItem

--- server/src/schemas/typeDefs.ts
+++ server/src/schemas/typeDefs.ts
@@ -6,27 +6,35 @@ email: String
 password: String
 }
 
+type Cart {
+id: ID
+customer: Customer
+items: [CartItem!]
+}
+
+type CartItem {
+product: Product!
+}
+
 type Auth {
 token: ID
-customer: Customer
- }
+customer: Customer}
 
 input CustomerInput {
 username: String!
 email: String!
-password: String!
- }
+password: String!}
 
 type Query {
 customers: [Customer]!
 customer(customerID: ID!): Customer
-me: Customer
- }
+me: Customer}
 
 type Mutation {
 addCustomer(input: CustomerInput!): Auth
 login(email: String!, password: String!): Auth
-
+addToCart(productId: ID!):Cart
+removeFromCart(productId: ID!):Cart
 removeCustomer: Customer
  }
 `;
