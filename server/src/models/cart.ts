7e291e4e3ed55e9972e3428e31075e42412a3850
Benjamin Machock <43613715+benjaminmachock@users.noreply.github.com>
2024-11-24 09:26:10 -0700

    updated cart model interfaces and fields

--- server/src/models/cart.ts
+++ server/src/models/cart.ts
@@ -1,18 +1,49 @@
-import { Schema, model, Document } from "mongoose";
+import { Schema, model, Document, Types } from "mongoose";
 
-interface iCart extends Document {
+interface Cart extends Document {
   _id: string;
-  productId: string;
-  orderId: string;
+  customer: Types.ObjectId;
+  items: CartItem[];
+  totalPrice: number;
+  paymentStatus: "processed" | "failed";
+  stripeSessionId?: string;
+}
+interface CartItem extends Document {
+  _id: string;
+  product: Types.ObjectId;
+  quantity: number;
+  price: number;
 }
 
-const cartSchema = new Schema<iCart>(
-  {
-    productId: {
-      type: String,
+const cartItemSchema = new Schema<CartItem>({
+  product: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
     },
-    orderId: {
+  ],
+  quantity: { type: Number, required: true, default: 1 },
+  price: { type: Number, required: true },
+});
+
+const cartSchema = new Schema<Cart>(
+  {
+    customer: {
+      type: Schema.Types.ObjectId,
+      ref: "Customer",
+      required: true,
+    },
+    items: [cartItemSchema],
+    totalPrice: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    stripeSessionId: { type: String },
+    paymentStatus: {
       type: String,
+      values: ["processed", "failed"],
     },
   },
   {
@@ -21,6 +52,6 @@ const cartSchema = new Schema<iCart>(
   }
 );
 
-const Cart = model<iCart>("Cart", cartSchema);
+const Cart = model<Cart>("Cart", cartSchema);
 
 export default Cart;
